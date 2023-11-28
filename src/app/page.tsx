7ee8ac86099093af55199d44a18337ae3f48ade2
haneeshbandaru20 <haneeshbandaru.project@gmail.com>
2023-11-28 21:34:06 +0530

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Homed from "@/components/home/homed";
 import About from "@/components/about/about";
 import Experience from "@/components/mytechstack/mytechstack";
 import Projects from "@/components/projects/projects";
-import Timelineevent from "@/components/timelineevent/timelineevent";
+// import Timelineevent from "@/components/timelineevent/timelineevent";
 
 export default function Home() {
   return (
@@ -13,7 +13,7 @@ export default function Home() {
       <Homed />
       <About />
       <Experience />
-      <Timelineevent />
+      {/* <Timelineevent /> */}
       <Projects />
     </main>
   );
