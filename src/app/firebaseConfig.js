d17c1d32794f41be47e8e19eca6de471f6c4b969
haneeshbandaru20 <haneeshbandaru.project@gmail.com>
2023-11-28 21:11:31 +0530

    fix

--- src/app/firebaseConfig.js
+++ src/app/firebaseConfig.js
@@ -18,24 +18,3 @@ const db = getFireStore(app)
 const analytics = getAnalytics(app);
 
 export {db};
-
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import {Firestore, getFireStore} from "firebase/firestore";
-
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAdaA-2um7_cRvQNHItrjdUUEU7zd-XKG0",
-  authDomain: "haneesh-portfolio.firebaseapp.com",
-  projectId: "haneesh-portfolio",
-  storageBucket: "haneesh-portfolio.appspot.com",
-  messagingSenderId: "307445660930",
-  appId: "1:307445660930:web:340a3dd21c3f343f04d61b",
-  measurementId: "G-1TSYTY2F64",
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFireStore(app)
-const analytics = getAnalytics(app);
-
-export {db};
