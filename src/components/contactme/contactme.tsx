80d2df0cd257a4f867e07587b5881a64cc75b0b8
haneeshbandaru20 <haneeshbandaru.project@gmail.com>
2023-11-29 12:57:43 +0530

    added contac me

---
+++ src/components/contactme/contactme.tsx
@@ -0,0 +1,48 @@
+import { Paper, TextField, Typography } from "@mui/material";
+import "./contactme.css";
+import mail from "../../assets/mail.svg";
+import Image from "next/image";
+import Link from "next/link";
+
+const contactme = () => {
+  return (
+    <div style={{ backgroundColor: "#9db2bf", justifyContent:"center" }}>
+      <div className="contact-main">
+        <div className="contact-text">
+          <h4>Get in Touch</h4>
+          <h1>Contact Me</h1>
+          <div className="contact-info-upper-container">
+            <div className="contact-info-container">
+              <Image
+                src={mail}
+                alt="Email icon"
+                className="icon contact-icon email-icon"
+              />
+              <p>
+                <a href="mailto:haneeshbandaru@gmail.com">
+                  haneeshbandaru@gmail.com
+                </a>
+              </p>
+            </div>
+            <div className="contact-info-container">
+              <Image
+                src={mail}
+                alt="LinkedIn icon"
+                className="icon contact-icon"
+              />
+              <p>
+                <a href="https://www.linkedin.com">LinkedIn</a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <footer>
+        <p className="contact-text-p">
+          Copyright ©️ 2023 Haneesh Bandaru. All Rights Reserved
+        </p>
+      </footer>
+    </div>
+  );
+};
+export default contactme;
--- src/components/timelineevent/timelineevent.tsx
+++ src/components/timelineevent/timelineevent.tsx
@@ -10,35 +10,7 @@ import "./timelineevent.css"; // Import your CSS file for styling
 const timelineevent = () => {
   return (
     <div className="timeline-main">
-      <Timeline position="alternate" >
-        <TimelineItem>
-          <TimelineSeparator>
-            <TimelineDot />
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent>Eat</TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineSeparator>
-            <TimelineDot />
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent>Code</TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineSeparator>
-            <TimelineDot />
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent>Sleep</TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineSeparator>
-            <TimelineDot />
-          </TimelineSeparator>
-          <TimelineContent>Repeat</TimelineContent>
-        </TimelineItem>
-      </Timeline>
+      
     </div>
   );
 };
