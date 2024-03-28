88601356190411d100cd1a04eb759917f66ae5b4
haneesh-bandaru <haneeshbandaru05@gmail.com>
2024-03-28 11:22:12 +0530

    added google analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,11 @@ import Homed from "@/components/home/homed";
 import About from "@/components/about/about";
 import Experience from "@/components/mytechstack/mytechstack";
 import Projects from "@/components/projects/projects";
-import Timelineevent from "@/components/timelineevent/timelineevent";
 import Contactme from "@/components/contactme/contactme";
-import Script from "next/script";
 
 export default function Home() {
   return (
     <main style={{ backgroundColor: "#DDE6ED" }}>
-      <Script />
       <NavBar />
       <Homed />
       <About />
